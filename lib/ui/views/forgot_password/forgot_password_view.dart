1c3e5170e1bd30813c1b7abefe28915ae765d4cf
suheerthedev <khansuheer1@gmail.com>
2025-04-08 17:05:49 +0500

    Completed Reset Pass Design

--- lib/ui/views/forgot_password/forgot_password_view.dart
+++ lib/ui/views/forgot_password/forgot_password_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:marketplace/app/app.router.dart';
 import 'package:marketplace/ui/common/app_colors.dart';
 import 'package:marketplace/ui/widgets/common/custom_button/custom_button.dart';
 import 'package:marketplace/ui/widgets/common/custom_text_field/custom_text_field.dart';
@@ -21,64 +22,63 @@ class ForgotPasswordView extends StackedView<ForgotPasswordViewModel> {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 0),
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              spacing: 25,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            spacing: 25,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Forgot password",
+                    style: GoogleFonts.hankenGrotesk(
+                        letterSpacing: -2,
+                        wordSpacing: 0,
+                        color: mainTextColor,
+                        fontSize: 36,
+                        fontWeight: FontWeight.w800),
+                  ),
+                  Text(
+                    "Enter your email to get reset password link",
+                    style: GoogleFonts.hankenGrotesk(
+                        color: lightTextColor,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400),
+                  ),
+                ],
+              ),
+              const CustomTextField(
+                  title: "Email",
+                  hintText: "Enter your email",
+                  hasSuffix: false),
+              CustomButton(
+                  title: "Send",
+                  onTap:
+                      viewModel.navigationSerivce.navigateToResetPasswordView,
+                  bgColor: mainBackgroundColor,
+                  textColor: secondaryTextColor,
+                  borderColor: lightBackgroundColor,
+                  hasIcon: false),
+              InkWell(
+                onTap: viewModel.navigationSerivce.back,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  spacing: 10,
                   children: [
+                    const Icon(Icons.arrow_back),
                     Text(
-                      "Forgot password",
+                      "Back to Login",
                       style: GoogleFonts.hankenGrotesk(
-                          letterSpacing: -2,
-                          wordSpacing: 0,
-                          color: mainTextColor,
-                          fontSize: 36,
-                          fontWeight: FontWeight.w800),
-                    ),
-                    Text(
-                      "Enter your email to get reset password link",
-                      style: GoogleFonts.hankenGrotesk(
-                          color: lightTextColor,
-                          fontSize: 16,
-                          fontWeight: FontWeight.w400),
-                    ),
+                        letterSpacing: -1,
+                        color: mainTextColor,
+                        fontWeight: FontWeight.w400,
+                        fontSize: 18,
+                      ),
+                    )
                   ],
                 ),
-                const CustomTextField(
-                    title: "Email",
-                    hintText: "Enter your email",
-                    hasSuffix: false),
-                CustomButton(
-                    title: "Send",
-                    onTap: () {},
-                    bgColor: mainBackgroundColor,
-                    textColor: secondaryTextColor,
-                    borderColor: lightBackgroundColor,
-                    hasIcon: false),
-                InkWell(
-                  onTap: viewModel.navigationSerivce.back,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    spacing: 10,
-                    children: [
-                      const Icon(Icons.arrow_back),
-                      Text(
-                        "Back to Login",
-                        style: GoogleFonts.hankenGrotesk(
-                          letterSpacing: -1,
-                          color: mainTextColor,
-                          fontWeight: FontWeight.w400,
-                          fontSize: 18,
-                        ),
-                      )
-                    ],
-                  ),
-                )
-              ],
-            ),
+              )
+            ],
           ),
         ),
       ),
--- lib/ui/views/reset_password/reset_password_view.dart
+++ lib/ui/views/reset_password/reset_password_view.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:marketplace/ui/common/app_colors.dart';
+import 'package:marketplace/ui/widgets/common/custom_button/custom_button.dart';
+import 'package:marketplace/ui/widgets/common/custom_text_field/custom_text_field.dart';
 import 'package:stacked/stacked.dart';
 
 import 'reset_password_viewmodel.dart';
@@ -13,10 +17,67 @@ class ResetPasswordView extends StackedView<ResetPasswordViewModel> {
     Widget? child,
   ) {
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.background,
-      body: Container(
-        padding: const EdgeInsets.only(left: 25.0, right: 25.0),
-        child: const Center(child: Text("ResetPasswordView")),
+      appBar: AppBar(),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            spacing: 25,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Reset Password",
+                    style: GoogleFonts.hankenGrotesk(
+                        letterSpacing: -2,
+                        wordSpacing: 0,
+                        color: mainTextColor,
+                        fontSize: 36,
+                        fontWeight: FontWeight.w800),
+                  ),
+                  Text(
+                    "Enter your new password",
+                    style: GoogleFonts.hankenGrotesk(
+                        color: lightTextColor,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w400),
+                  ),
+                ],
+              ),
+              CustomTextField(
+                title: "New Password",
+                hintText: "Enter your password",
+                hasSuffix: true,
+                suffixIcon: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.visibility_off_outlined,
+                      color: iconColor,
+                    )),
+              ),
+              CustomTextField(
+                title: "Re-enter Password",
+                hintText: "Enter your password",
+                hasSuffix: true,
+                suffixIcon: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.visibility_off_outlined,
+                      color: iconColor,
+                    )),
+              ),
+              CustomButton(
+                  title: "Save",
+                  onTap: () {},
+                  bgColor: mainBackgroundColor,
+                  textColor: secondaryTextColor,
+                  borderColor: lightBackgroundColor,
+                  hasIcon: false),
+            ],
+          ),
+        ),
       ),
     );
   }
