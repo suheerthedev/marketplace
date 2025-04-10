48bf922410fb951f42119417dee858fb83633903
suheerthedev <khansuheer1@gmail.com>
2025-04-10 15:08:37 +0500

    Removed Unnesscery Imports

--- lib/ui/views/main/main_view.dart
+++ lib/ui/views/main/main_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:marketplace/ui/common/app_colors.dart';
 import 'package:marketplace/ui/views/account/account_view.dart';
 import 'package:marketplace/ui/views/cart/cart_view.dart';
