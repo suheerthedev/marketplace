4880e8132608b232f2e48bdf2ab83b8c0efd038e
suheerthedev <khansuheer1@gmail.com>
2025-04-11 21:04:32 +0500

    Completed Seller Help Center View

--- lib/ui/views/seller_help_center/seller_help_center_viewmodel.dart
+++ lib/ui/views/seller_help_center/seller_help_center_viewmodel.dart
@@ -1,3 +1,10 @@
 import 'package:stacked/stacked.dart';
 
-class SellerHelpCenterViewModel extends BaseViewModel {}
+class SellerHelpCenterViewModel extends BaseViewModel {
+  List<String> questions = [
+    'How do I create a seller account?',
+    'What are the fees for listing products?',
+    'How will I know when I receive an order?',
+    'Can I edit or delete a product after listing it?',
+  ];
+}
