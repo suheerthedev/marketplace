dcb79a2fe34366c0937f95f06dca32ef7fed18e6
suheerthedev <khansuheer1@gmail.com>
2025-04-14 15:39:28 +0500

    Completed Notification View

--- lib/ui/views/notification/notification_view.dart
+++ lib/ui/views/notification/notification_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:marketplace/ui/common/app_colors.dart';
+import 'package:marketplace/ui/widgets/common/main_app_bar/main_app_bar.dart';
 import 'package:stacked/stacked.dart';
 
 import 'notification_viewmodel.dart';
@@ -13,10 +16,74 @@ class NotificationView extends StackedView<NotificationViewModel> {
     Widget? child,
   ) {
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.background,
-      body: Container(
-        padding: const EdgeInsets.only(left: 25.0, right: 25.0),
-        child: const Center(child: Text("NotificationView")),
+      appBar: const MainAppBar(title: "Notifications"),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: _buildNotificationList(viewModel),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildNotificationList(NotificationViewModel viewModel) {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16),
+      itemCount: 4,
+      itemBuilder: (context, index) {
+        return _buildNotificationItem(
+            userName: "Thomas Jepkins",
+            content: 'liked product 3',
+            time: '4 min',
+            imageUrl: 'assets/images/profile.png',
+            onTap: () {});
+      },
+    );
+  }
+
+  Widget _buildNotificationItem({
+    required String userName,
+    required String content,
+    required String time,
+    required String imageUrl,
+    required VoidCallback onTap,
+  }) {
+    return ListTile(
+      onTap: onTap,
+      contentPadding: const EdgeInsets.symmetric(horizontal: 8),
+      leading: CircleAvatar(
+        radius: 20,
+        backgroundImage: AssetImage(imageUrl),
+        backgroundColor: lightContainerColor,
+        child: AssetImage(imageUrl).toString().isEmpty
+            ? const Icon(Icons.person, color: mainTextColor)
+            : null,
+      ),
+      title: Text(
+        userName,
+        style: GoogleFonts.roboto(
+          color: mainBackgroundColor,
+          fontSize: 15,
+          fontWeight: FontWeight.w500,
+        ),
+      ),
+      subtitle: Text(
+        content,
+        maxLines: 2,
+        overflow: TextOverflow.ellipsis,
+        style: GoogleFonts.roboto(
+          color: mainBackgroundColor,
+          fontSize: 12,
+          fontWeight: FontWeight.w300,
+        ),
+      ),
+      trailing: Text(
+        time,
+        style: GoogleFonts.roboto(
+            fontSize: 10, fontWeight: FontWeight.w400, color: lightTextColor),
       ),
     );
   }
