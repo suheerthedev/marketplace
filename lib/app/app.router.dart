3789812210a66ff5d3a2b9f5198e972da6a24b3c
suheerthedev <khansuheer1@gmail.com>
2025-04-15 16:14:03 +0500

    Created OrdersView

--- lib/app/app.router.dart
+++ lib/app/app.router.dart
@@ -5,9 +5,9 @@
 // **************************************************************************
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:flutter/material.dart' as _i35;
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart' as _i34;
-import 'package:marketplace/models/product_model.dart' as _i35;
+import 'package:marketplace/models/product_model.dart' as _i36;
 import 'package:marketplace/ui/views/account/account_view.dart' as _i12;
 import 'package:marketplace/ui/views/buyer_login/buyer_login_view.dart' as _i5;
 import 'package:marketplace/ui/views/buyer_profile/buyer_profile_view.dart'
@@ -25,6 +25,7 @@ import 'package:marketplace/ui/views/home/home_view.dart' as _i2;
 import 'package:marketplace/ui/views/main/main_view.dart' as _i8;
 import 'package:marketplace/ui/views/notification/notification_view.dart'
     as _i25;
+import 'package:marketplace/ui/views/orders/orders_view.dart' as _i34;
 import 'package:marketplace/ui/views/pay_now/pay_now_view.dart' as _i30;
 import 'package:marketplace/ui/views/product_detail/product_detail_view.dart'
     as _i9;
@@ -61,7 +62,7 @@ import 'package:marketplace/ui/views/startup/startup_view.dart' as _i3;
 import 'package:marketplace/ui/views/verification/verification_view.dart'
     as _i32;
 import 'package:stacked/stacked.dart' as _i1;
-import 'package:stacked_services/stacked_services.dart' as _i36;
+import 'package:stacked_services/stacked_services.dart' as _i37;
 
 class Routes {
   static const homeView = '/home-view';
@@ -128,6 +129,8 @@ class Routes {
 
   static const buyerProfileView = '/buyer-profile-view';
 
+  static const ordersView = '/orders-view';
+
   static const all = <String>{
     homeView,
     startupView,
@@ -161,6 +164,7 @@ class Routes {
     confirmationView,
     verificationView,
     buyerProfileView,
+    ordersView,
   };
 }
 
@@ -294,200 +298,210 @@ class StackedRouter extends _i1.RouterBase {
       Routes.buyerProfileView,
       page: _i33.BuyerProfileView,
     ),
+    _i1.RouteDef(
+      Routes.ordersView,
+      page: _i34.OrdersView,
+    ),
   ];
 
   final _pagesMap = <Type, _i1.StackedRouteFactory>{
     _i2.HomeView: (data) {
-      return _i34.MaterialPageRoute<dynamic>(
+      return _i35.MaterialPageRoute<dynamic>(
         builder: (context) => const _i2.HomeView(),
         settings: data,
       );
     },
     _i3.StartupView: (data) {
-      return _i34.MaterialPageRoute<dynamic>(
+      return _i35.MaterialPageRoute<dynamic>(
         builder: (context) => const _i3.StartupView(),
         settings: data,
       );
     },
     _i4.BuyerSignUpView: (data) {
-      return _i34.MaterialPageRoute<dynamic>(
+      return _i35.MaterialPageRoute<dynamic>(
         builder: (context) => const _i4.BuyerSignUpView(),
         settings: data,
       );
     },
     _i5.BuyerLoginView: (data) {
-      return _i34.MaterialPageRoute<dynamic>(
+      return _i35.MaterialPageRoute<dynamic>(
         builder: (context) => const _i5.BuyerLoginView(),
         settings: data,
       );
     },
     _i6.ForgotPasswordView: (data) {
-      return _i34.MaterialPageRoute<dynamic>(
+      return _i35.MaterialPageRoute<dynamic>(
         builder: (context) => const _i6.ForgotPasswordView(),
         settings: data,
       );
     },
     _i7.ResetPasswordView: (data) {
-      return _i34.MaterialPageRoute<dynamic>(
+      return _i35.MaterialPageRoute<dynamic>(
         builder: (context) => const _i7.ResetPasswordView(),
         settings: data,
       );
     },
     _i8.MainView: (data) {
-      return _i34.MaterialPageRoute<dynamic>(
+      return _i35.MaterialPageRoute<dynamic>(
         builder: (context) => const _i8.MainView(),
         settings: data,
       );
     },
     _i9.ProductDetailView: (data) {
       final args = data.getArgs<ProductDetailViewArguments>(nullOk: false);
-      return _i34.MaterialPageRoute<dynamic>(
+      return _i35.MaterialPageRoute<dynamic>(
         builder: (context) =>
             _i9.ProductDetailView(product: args.product, key: args.key),
         settings: data,
       );
     },
     _i10.CartView: (data) {
-      return _i34.MaterialPageRoute<dynamic>(
+      return _i35.MaterialPageRoute<dynamic>(
         builder: (context) => const _i10.CartView(),
         settings: data,
       );
     },
     _i11.SavedView: (data) {
-      return _i34.MaterialPageRoute<dynamic>(
+      return _i35.MaterialPageRoute<dynamic>(
         builder: (context) => const _i11.SavedView(),
         settings: data,
       );
     },
     _i12.AccountView: (data) {
-      return _i34.MaterialPageRoute<dynamic>(
+      return _i35.MaterialPageRoute<dynamic>(
         builder: (context) => const _i12.AccountView(),
         settings: data,
       );
     },
     _i13.SellerSignUpView: (data) {
-      return _i34.MaterialPageRoute<dynamic>(
+      return _i35.MaterialPageRoute<dynamic>(
         builder: (context) => const _i13.SellerSignUpView(),
         settings: data,
       );
     },
     _i14.SellerDashboardView: (data) {
-      return _i34.MaterialPageRoute<dynamic>(
+      return _i35.MaterialPageRoute<dynamic>(
         builder: (context) => const _i14.SellerDashboardView(),
         settings: data,
       );
     },
     _i15.SellerProductsView: (data) {
-      return _i34.MaterialPageRoute<dynamic>(
+      return _i35.MaterialPageRoute<dynamic>(
         builder: (context) => const _i15.SellerProductsView(),
         settings: data,
       );
     },
     _i16.SellerMainView: (data) {
-      return _i34.MaterialPageRoute<dynamic>(
+      return _i35.MaterialPageRoute<dynamic>(
         builder: (context) => const _i16.SellerMainView(),
         settings: data,
       );
     },
     _i17.SellerInboxView: (data) {
-      return _i34.MaterialPageRoute<dynamic>(
+      return _i35.MaterialPageRoute<dynamic>(
         builder: (context) => const _i17.SellerInboxView(),
         settings: data,
       );
     },
     _i18.SellerAccountView: (data) {
-      return _i34.MaterialPageRoute<dynamic>(
+      return _i35.MaterialPageRoute<dynamic>(
         builder: (context) => const _i18.SellerAccountView(),
         settings: data,
       );
     },
     _i19.SellerChatView: (data) {
-      return _i34.MaterialPageRoute<dynamic>(
+      return _i35.MaterialPageRoute<dynamic>(
         builder: (context) => const _i19.SellerChatView(),
         settings: data,
       );
     },
     _i20.SellerProfileView: (data) {
-      return _i34.MaterialPageRoute<dynamic>(
+      return _i35.MaterialPageRoute<dynamic>(
         builder: (context) => const _i20.SellerProfileView(),
         settings: data,
       );
     },
     _i21.SellerMyWalletView: (data) {
-      return _i34.MaterialPageRoute<dynamic>(
+      return _i35.MaterialPageRoute<dynamic>(
         builder: (context) => const _i21.SellerMyWalletView(),
         settings: data,
       );
     },
     _i22.SellerHelpCenterView: (data) {
-      return _i34.MaterialPageRoute<dynamic>(
+      return _i35.MaterialPageRoute<dynamic>(
         builder: (context) => const _i22.SellerHelpCenterView(),
         settings: data,
       );
     },
     _i23.SellerEditProductView: (data) {
-      return _i34.MaterialPageRoute<dynamic>(
+      return _i35.MaterialPageRoute<dynamic>(
         builder: (context) => const _i23.SellerEditProductView(),
         settings: data,
       );
     },
     _i24.SellerAddProductView: (data) {
-      return _i34.MaterialPageRoute<dynamic>(
+      return _i35.MaterialPageRoute<dynamic>(
         builder: (context) => const _i24.SellerAddProductView(),
         settings: data,
       );
     },
     _i25.NotificationView: (data) {
-      return _i34.MaterialPageRoute<dynamic>(
+      return _i35.MaterialPageRoute<dynamic>(
         builder: (context) => const _i25.NotificationView(),
         settings: data,
       );
     },
     _i26.SellerTrendingProductsView: (data) {
-      return _i34.MaterialPageRoute<dynamic>(
+      return _i35.MaterialPageRoute<dynamic>(
         builder: (context) => const _i26.SellerTrendingProductsView(),
         settings: data,
       );
     },
     _i27.SellerInvoiceDetailView: (data) {
-      return _i34.MaterialPageRoute<dynamic>(
+      return _i35.MaterialPageRoute<dynamic>(
         builder: (context) => const _i27.SellerInvoiceDetailView(),
         settings: data,
       );
     },
     _i28.CategoryView: (data) {
-      return _i34.MaterialPageRoute<dynamic>(
+      return _i35.MaterialPageRoute<dynamic>(
         builder: (context) => const _i28.CategoryView(),
         settings: data,
       );
     },
     _i29.CheckoutView: (data) {
-      return _i34.MaterialPageRoute<dynamic>(
+      return _i35.MaterialPageRoute<dynamic>(
         builder: (context) => const _i29.CheckoutView(),
         settings: data,
       );
     },
     _i30.PayNowView: (data) {
-      return _i34.MaterialPageRoute<dynamic>(
+      return _i35.MaterialPageRoute<dynamic>(
         builder: (context) => const _i30.PayNowView(),
         settings: data,
       );
     },
     _i31.ConfirmationView: (data) {
-      return _i34.MaterialPageRoute<dynamic>(
+      return _i35.MaterialPageRoute<dynamic>(
         builder: (context) => const _i31.ConfirmationView(),
         settings: data,
       );
     },
     _i32.VerificationView: (data) {
-      return _i34.MaterialPageRoute<dynamic>(
+      return _i35.MaterialPageRoute<dynamic>(
         builder: (context) => const _i32.VerificationView(),
         settings: data,
       );
     },
     _i33.BuyerProfileView: (data) {
-      return _i34.MaterialPageRoute<dynamic>(
+      return _i35.MaterialPageRoute<dynamic>(
         builder: (context) => const _i33.BuyerProfileView(),
+        settings: data,
+      );
+    },
+    _i34.OrdersView: (data) {
+      return _i35.MaterialPageRoute<dynamic>(
+        builder: (context) => const _i34.OrdersView(),
         settings: data,
       );
     },
@@ -506,9 +520,9 @@ class ProductDetailViewArguments {
     this.key,
   });
 
-  final _i35.Product product;
+  final _i36.Product product;
 
-  final _i34.Key? key;
+  final _i35.Key? key;
 
   @override
   String toString() {
@@ -527,7 +541,7 @@ class ProductDetailViewArguments {
   }
 }
 
-extension NavigatorStateExtension on _i36.NavigationService {
+extension NavigatorStateExtension on _i37.NavigationService {
   Future<dynamic> navigateToHomeView([
     int? routerId,
     bool preventDuplicates = true,
@@ -627,8 +641,8 @@ extension NavigatorStateExtension on _i36.NavigationService {
   }
 
   Future<dynamic> navigateToProductDetailView({
-    required _i35.Product product,
-    _i34.Key? key,
+    required _i36.Product product,
+    _i35.Key? key,
     int? routerId,
     bool preventDuplicates = true,
     Map<String, String>? parameters,
@@ -979,6 +993,20 @@ extension NavigatorStateExtension on _i36.NavigationService {
         transition: transition);
   }
 
+  Future<dynamic> navigateToOrdersView([
+    int? routerId,
+    bool preventDuplicates = true,
+    Map<String, String>? parameters,
+    Widget Function(BuildContext, Animation<double>, Animation<double>, Widget)?
+        transition,
+  ]) async {
+    return navigateTo<dynamic>(Routes.ordersView,
+        id: routerId,
+        preventDuplicates: preventDuplicates,
+        parameters: parameters,
+        transition: transition);
+  }
+
   Future<dynamic> replaceWithHomeView([
     int? routerId,
     bool preventDuplicates = true,
@@ -1078,8 +1106,8 @@ extension NavigatorStateExtension on _i36.NavigationService {
   }
 
   Future<dynamic> replaceWithProductDetailView({
-    required _i35.Product product,
-    _i34.Key? key,
+    required _i36.Product product,
+    _i35.Key? key,
     int? routerId,
     bool preventDuplicates = true,
     Map<String, String>? parameters,
@@ -1424,6 +1452,20 @@ extension NavigatorStateExtension on _i36.NavigationService {
         transition,
   ]) async {
     return replaceWith<dynamic>(Routes.buyerProfileView,
+        id: routerId,
+        preventDuplicates: preventDuplicates,
+        parameters: parameters,
+        transition: transition);
+  }
+
+  Future<dynamic> replaceWithOrdersView([
+    int? routerId,
+    bool preventDuplicates = true,
+    Map<String, String>? parameters,
+    Widget Function(BuildContext, Animation<double>, Animation<double>, Widget)?
+        transition,
+  ]) async {
+    return replaceWith<dynamic>(Routes.ordersView,
         id: routerId,
         preventDuplicates: preventDuplicates,
         parameters: parameters,
