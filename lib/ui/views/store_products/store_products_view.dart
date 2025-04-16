8443fd073fc3bb8df235ce1dbd4337e6130187fb
suheerthedev <khansuheer1@gmail.com>
2025-04-16 21:35:32 +0500

    Completed Store Products View

--- lib/ui/views/store_products/store_products_view.dart
+++ lib/ui/views/store_products/store_products_view.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:marketplace/ui/widgets/common/product_card_1/product_card_1.dart';
+import 'package:marketplace/ui/widgets/store/store_app_bar/store_app_bar.dart';
+import 'package:marketplace/ui/widgets/store/store_header/store_header.dart';
 import 'package:stacked/stacked.dart';
 
 import 'store_products_viewmodel.dart';
@@ -13,10 +17,64 @@ class StoreProductsView extends StackedView<StoreProductsViewModel> {
     Widget? child,
   ) {
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.background,
-      body: Container(
-        padding: const EdgeInsets.only(left: 25.0, right: 25.0),
-        child: const Center(child: Text("StoreProductsView")),
+      appBar: StoreAppBar(
+        onSearchTap: viewModel.onSearchTap,
+        onCartTap: viewModel.onCartTap,
+        onNotificationTap: viewModel.onNotificationTap,
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                StoreHeader(
+                  storeName: "Store Name",
+                  rating: "4.5",
+                  isFollowing: true,
+                  onFollowTap: viewModel.onFollowTap,
+                ),
+                const SizedBox(height: 16),
+                _buildSectionHeader("Products"),
+                const SizedBox(height: 16),
+                GridView.builder(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    childAspectRatio: 0.7,
+                    crossAxisSpacing: 15,
+                    mainAxisSpacing: 0,
+                  ),
+                  itemCount: viewModel.products.length,
+                  itemBuilder: (context, index) {
+                    final product = viewModel.products[index];
+
+                    return ProductCard1(
+                      product: product,
+                      isTappable: true,
+                      onTapped: () {
+                        viewModel.onProductTap(product);
+                      },
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSectionHeader(String title) {
+    return Text(
+      title,
+      style: GoogleFonts.roboto(
+        fontSize: 16,
+        fontWeight: FontWeight.w600,
+        color: Colors.black87,
       ),
     );
   }
--- lib/ui/views/store_products/store_products_viewmodel.dart
+++ lib/ui/views/store_products/store_products_viewmodel.dart
@@ -1,3 +1,22 @@
+import 'package:marketplace/app/app.locator.dart';
+import 'package:marketplace/app/app.router.dart';
+import 'package:marketplace/models/product_model.dart';
+import 'package:marketplace/services/product_service.dart';
 import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
 
-class StoreProductsViewModel extends BaseViewModel {}
+class StoreProductsViewModel extends BaseViewModel {
+  final _navigationService = locator<NavigationService>();
+
+  final ProductService productService = locator<ProductService>();
+
+  List<Product> get products => productService.products;
+  void onProductTap(Product product) {
+    _navigationService.navigateToProductDetailView(product: product);
+  }
+
+  void onSearchTap() {}
+  void onCartTap() {}
+  void onNotificationTap() {}
+  void onFollowTap() {}
+}
