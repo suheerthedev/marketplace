6e33941aff2d6cc7c9ca3060706d932f1fbdfef0
suheerthedev <khansuheer1@gmail.com>
2025-04-16 18:32:53 +0500

    Completed Term And Conditions View

--- lib/ui/views/terms_conditons/terms_conditons_view.dart
+++ lib/ui/views/terms_conditons/terms_conditons_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:marketplace/ui/common/app_colors.dart';
 import 'package:marketplace/ui/widgets/common/main_app_bar/main_app_bar.dart';
 import 'package:stacked/stacked.dart';
 
@@ -15,9 +17,66 @@ class TermsConditonsView extends StackedView<TermsConditonsViewModel> {
   ) {
     return Scaffold(
       appBar: const MainAppBar(title: "Terms & Conditions"),
-      body: Container(
-        padding: const EdgeInsets.only(left: 25.0, right: 25.0),
-        child: const Center(child: Text("TermsConditonsView")),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.",
+                style: GoogleFonts.roboto(
+                  fontSize: 14,
+                  letterSpacing: -0.2,
+                  height: 1.5,
+                  fontWeight: FontWeight.w300,
+                  color: mainTextColor,
+                ),
+              ),
+              const SizedBox(height: 20),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: viewModel.items.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 12.0),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                            child: Text(
+                              "${index + 1}.",
+                              style: GoogleFonts.roboto(
+                                fontSize: 14,
+                                letterSpacing: -0.2,
+                                height: 1.5,
+                                fontWeight: FontWeight.w300,
+                                color: mainTextColor,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(width: 6),
+                          Expanded(
+                            child: Text(
+                              viewModel.items[index],
+                              style: GoogleFonts.roboto(
+                                fontSize: 14,
+                                letterSpacing: -0.2,
+                                height: 1.5,
+                                fontWeight: FontWeight.w300,
+                                color: mainTextColor,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/ui/views/terms_conditons/terms_conditons_viewmodel.dart
+++ lib/ui/views/terms_conditons/terms_conditons_viewmodel.dart
@@ -1,3 +1,8 @@
 import 'package:stacked/stacked.dart';
 
-class TermsConditonsViewModel extends BaseViewModel {}
+class TermsConditonsViewModel extends BaseViewModel {
+  final List<String> items = [
+    "Introduction\nbut also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset.",
+    "Conditions of Use\nsheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  ];
+}
